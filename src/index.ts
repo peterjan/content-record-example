4afa07e62348ab55a25077ae99c80ea2344ca948
PJ <peterjan.brone@gmail.com>
2021-04-08 16:00:26 +0200

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -13,41 +13,41 @@ export default class ExampleSkapp {
   private crDAC: ContentRecordDAC;
 
   constructor(public client: SkynetClient) {
-    this.crDAC = new ContentRecordDAC()
+    this.crDAC = new ContentRecordDAC();
     this.list = document.getElementById("ulContentList") as HTMLElement;
     this.listCount = 0;
   }
 
   public async init(): Promise<void> {
     // load mysky
-    this.mySky = await this.client.loadMySky(SKAPP_DOMAIN)
+    this.mySky = await this.client.loadMySky(SKAPP_DOMAIN);
 
     // load DACs
-    await this.mySky.loadDacs(this.crDAC)
+    await this.mySky.loadDacs(this.crDAC);
 
     // check login
     const successfulLogin = await this.mySky.checkLogin();
-    console.log('login succeeded', successfulLogin)
+    console.log("login succeeded", successfulLogin);
   }
 
   public async login(): Promise<void> {
-    console.log('try login...')
-    const successfulLogin = await this.mySky.requestLoginAccess()
-    console.log('login succeeded', successfulLogin)
+    console.log("try login...");
+    const successfulLogin = await this.mySky.requestLoginAccess();
+    console.log("login succeeded", successfulLogin);
   }
 
   public async create(): Promise<void> {
     const cntr = this.listCount++;
 
     // upload content -> generates skylink
-    const skylink = randomSkylink()
+    const skylink = randomSkylink();
 
     // create list item
-    const item = document.createElement('li');
+    const item = document.createElement("li");
     const itemId = `btnInteract${cntr}`;
-    item.className="contentItem"
+    item.className = "contentItem";
     item.innerHTML = `
-      <p>${cntr+1}. I am a piece of content</p>
+      <p>${cntr + 1}. I am a piece of content</p>
       <input type="button" id="${itemId}" data-id=${cntr} data-skylink=${skylink} value="Like Me"/>
     `;
     this.list.appendChild(item);
@@ -58,22 +58,30 @@ export default class ExampleSkapp {
     // record create
     const create = {
       content: skylink,
-      metadata: { identifier: cntr }
-    }
-    console.log('recording creation', create)
+      metadata: { identifier: cntr },
+    };
+    console.log("recording creation", create);
     const result = await this.crDAC.recordNewContent(create);
-    console.log(result)
+    console.log(result);
   }
 
+  public async tmp() {
+    try {
+      const result = await this.mySky.getJSON("somepath");
+      console.log("result", result);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
   public async like(e: MouseEvent): Promise<void> {
     const element = e.target as HTMLElement;
     const interaction = {
       content: element.dataset.skylink as string,
-      metadata: { identifier: element.dataset.id }
-    }
-    console.log('recording interaction', interaction)
+      metadata: { identifier: element.dataset.id },
+    };
+    console.log("recording interaction", interaction);
     const result = await this.crDAC.recordInteraction(interaction);
-    console.log(result)
+    console.log(result);
   }
 }
 
@@ -82,10 +90,11 @@ let skapp: ExampleSkapp;
 (async () => {
   const client = new SkynetClient("https://siasky.net");
   skapp = new ExampleSkapp(client);
-  await skapp.init()
-  console.log('ExampleSkapp initialized');
-    
+  await skapp.init();
+  console.log("ExampleSkapp initialized");
+
   // bind functions
   document.getElementById("btnCreate")!.onclick = skapp.create.bind(skapp);
   document.getElementById("btnLogin")!.onclick = skapp.login.bind(skapp);
+  document.getElementById("btnTmp")!.onclick = skapp.tmp.bind(skapp);
 })();
